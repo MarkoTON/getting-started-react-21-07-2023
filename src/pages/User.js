f9d7a60f40c19326d3219be842e9ed6dae6bce1f
markoton <markotoncanovic@gmail.com>
2023-07-27 12:24:05 +0200

    initial setup of users

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,7 +1,15 @@
+import { useParams } from "react-router-dom";
+import useFetchData from "../hooks/useFetchData";
+
 function User() {
+  const { id } = useParams()
+  const { data, loading } = useFetchData(`https://jsonplaceholder.typicode.com/users/${id}`); // Zamijenite sa stvarnim URL-om API-ja
+
   return ( 
     <div>
-      User
+      User - {id}
+      <hr />
+      {data && data.name}
     </div>
    );
 }
